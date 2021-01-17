5e327edd9d381ef24a70e5aabb462adfcd76c1c4
deskreject <staub_alex@yahoo.com>
2021-01-17 20:04:24 +0100

    finished the code for exercise 2 of the final programming challenge

--- Script/course_2_week_4/best_hospital.R
+++ Script/course_2_week_4/best_hospital.R
@@ -1,5 +1,7 @@
 ##### ------------------- package loading and data reading ---------------- #####
 
+Sys.setenv(LANG = "en")
+
 library(tidyverse)
 if (!require(tidyverse)) install.packages("tidyverse"); library(tidyverse)  
 if (!require(here)) install.packages("here"); library(here)  
@@ -41,17 +43,52 @@ best <- function(state, outcome) {
   ## Read outcome data
   df <- read.csv(here::here("Data",
                             "course_2_week_4",
-                            "hospital-data.csv"))
+                            "outcome-of-care-measures.csv"))
   
   ## Check that state and outcome are valid
   
+  potential_outcomes <- c("heart attack", "heart failure", "pneumonia")
+  
+  if(!state %in% df$State){
+    stop("state name not found")
+  }
+  
+  if(!outcome %in% potential_outcomes){
+    stop("outcome name not found")
+  }
   
   ## Return hospital name in that state with lowest 30-day death
-  best_hospital <- if(outcome == "hear attack") {
-  min(best_hospital[,11])
+  best_hospital <- if(outcome == "heart attack") {
+  df %>% filter(state == State) %>%
+      filter(Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack == min(Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack, 
+                                                                              na.rm = T)) %>%
+      select(Hospital.Name)
     
+  } else if(outcome == "heart failure"){
+    df %>% filter(state == State) %>%
+      filter(as.numeric(Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure)  == min(as.numeric(Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure), 
+                                                                                na.rm = T)) %>%
+      select(Hospital.Name)
+  } else if(outcome == "pneumonia"){
+    df %>% filter(state == State) %>%
+      filter(as.numeric(Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia )  == min(as.numeric(Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia ), 
+                                                                                                      na.rm = T)) %>%
+      select(Hospital.Name)
   }
   
   ## rate
-  best_hospital
+  return(sort(best_hospital$Hospital.Name))
 }
+
+# test the function
+best("TX", "heart failure")
+
+best("NY", "heart attack")
+
+best("MD", "heart attack")
+
+best("MD", "pneumonia")
+
+best("BB", "heart attack")
+
+best("NY", "hert attack")
